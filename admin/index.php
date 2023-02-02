32987f8ed166a8e9280a2f9a270ac5ff6e3b9a3e
official-kalyani <kpriyadarsini330@gmail.com>
2023-02-02 17:15:30 +0530

    Laravel Hospital management system project

---
+++ admin/index.php
@@ -0,0 +1,209 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<?php include 'includes/header.php' ?>
+<link rel="stylesheet" href="../css/c3.min.css">
+
+<body class="materialdesign"><div class="wrapper-pro">
+
+    <?php include 'includes/sidebar.php' ?>
+    <?php include 'includes/topbar.php' ?>
+
+    <!-- Breadcome start-->
+    <div class="breadcome-area mg-b-30 small-dn">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="breadcome-list map-mg-t-40-gl shadow-reset">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="breadcome-heading">
+                                    <form role="search" class="">
+                                        <input type="text" placeholder="Search..." class="form-control">
+                                        <a href=""><i class="fa fa-search"></i></a>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <ul class="breadcome-menu">
+                                    <li><a href="#">Home</a> <span class="bread-slash">/</span>
+                                    </li>
+                                    <li><span class="bread-blod">Dashboard</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcome End-->
+    <!-- Breadcome start-->
+    <div class="breadcome-area des-none mg-b-30">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="breadcome-list map-mg-t-40-gl shadow-reset">
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                <div class="breadcome-heading">
+                                    <form role="search" class="">
+                                        <input type="text" placeholder="Search..." class="form-control">
+                                        <a href=""><i class="fa fa-search"></i></a>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                                <ul class="breadcome-menu">
+                                    <li><a href="#">Home</a> <span class="bread-slash">/</span>
+                                    </li>
+                                    <li><span class="bread-blod">Dashboard</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Breadcome End-->
+    <!-- income order visit user Start -->
+    <div class="income-order-visit-user-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Files</h2>
+                                <div class="main-income-phara">
+                                <p><i class="fa big-icon fa-folder fa-2x"></i></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3><span class="counter">120</span></h3>
+                                </div>
+                                <div class="price-graph">
+                                    <span id="sparkline1"></span>
+                                </div>
+                            </div>
+                            <div class="income-range">
+                                <p>Number of Files</p>
+                                <span class="income-percentange">98% <i class="fa fa-folder"></i></span>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Users</h2>
+                                <div class="main-income-phara order-cl">
+                                <p><i class="fa big-icon fa-users fa-2x"></i></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3><span class="counter">50</span></h3>
+                                </div>
+                                <div class="price-graph">
+                                    <span id="sparkline6"></span>
+                                </div>
+                            </div>
+                            <div class="income-range order-cl">
+                                <p>Number of Users</p>
+                                <span class="income-percentange">66% <i class="fa fa-user"></i></span>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Categories</h2>
+                                <div class="main-income-phara visitor-cl">
+                                <p><i class="fa big-icon fa-th-list fa-2x"></i></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3><span class="counter">25</span></h3>
+                                </div>
+                                <div class="price-graph">
+                                    <span id="sparkline2"></span>
+                                </div>
+                            </div>
+                            <div class="income-range visitor-cl">
+                                <p>Number of File Categories</p>
+                                <span class="income-percentange">55% <i class="fa fa-file"></i></span>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Departments</h2>
+                                <div class="main-income-phara low-value-cl">
+                                    <p><i class="fa big-icon fa-building fa-2x"></i></p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3><span class="counter">10</span></h3>
+                                </div>
+                                <div class="price-graph">
+                                    <span id="sparkline5"></span>
+                                </div>
+                            </div>
+                            <div class="income-range low-value-cl">
+                                <p>Number of Departments</p>
+                                <span class="income-percentange">33% <i class="fa fa-building"></i></span>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>            
+                <div class="col-lg-12">
+                    <div class="sparkline8-list shadow-reset">
+                        <div class="sparkline8-hd">
+                            <div class="main-sparkline8-hd">
+                                <h1>Number of file by Department</h1>
+                            </div>
+                            </div>
+                            <div class="sparkline8-graph">
+                            <div id="stocked"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+
+
+    <?php include 'includes/footer.php' ?>
+    <script src="../js/c3-charts/d3.min.js"></script>
+    <script src="../js/c3-charts/c3.min.js"></script>
+    <script src="../js/c3-charts/c3-active.js"></script>
+</body>
+
+</html>
